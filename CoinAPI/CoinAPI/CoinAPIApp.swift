fab2f74902e5437b0bcbbb231a2985f528c44340
Stefan Bayne <stefanbayne@gmail.com>
2022-11-17 13:11:56 -0500

    First commit.

---
+++ CoinAPI/CoinAPI/CoinAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAPIApp.swift
+//  CoinAPI
+//
+//  Created by Stefan Bayne on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
